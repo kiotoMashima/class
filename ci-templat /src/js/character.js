085935a18f51ef2954904cb8f3db4e2b089a3c27
Mishima <modernden4@gmail.com>
2024-07-06 19:17:56 +0300

    sss

--- ci-templat /src/js/character.js
+++ ci-templat /src/js/character.js
@@ -12,21 +12,21 @@ export default class Character{
       this.health = 100;
       this.level = 1;
     }
-  }
-  function levelUP(character){
-    if(character.health > 0){
-      character.level++;
-      character.attack = character.attack + character.attack * 0.2;
-      character.defence = character.defence + character.defence * 0.2;
-      character.health = 100;
-    } else{
-      throw new Error("Не можешь повысить уровень умершего");
-    }
-  };
-  function damage(character, point){
-    if(character.health > 0){
-      character.health = character.health - point * (1 - character.defence / 100);
-    } else{
-      throw new Error("Не можешь нанести урон умершему");
+    levelUP(){
+      if(character.health > 0){
+        character.level++;
+        character.attack = character.attack + character.attack * 0.2;
+        character.defence = character.defence + character.defence * 0.2;
+        character.health = 100;
+      } else{
+        throw new Error("Не можешь повысить уровень умершего");
+      }
+    };
+    damage(point){
+      if(character.health > 0){
+        character.health = character.health - point * (1 - character.defence / 100);
+      } else{
+        throw new Error("Не можешь нанести урон умершему");
+      }
     }
   }
